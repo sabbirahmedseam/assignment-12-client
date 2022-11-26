7f9ed5bb78ef4ada0b174affe5ff29712cb05e1f
sabbirahmedseam <sabbirahmedseam1@gmail.com>
2022-11-27 02:57:37 +0600

    send

--- src/Home/Categories.js
+++ src/Home/Categories.js
@@ -22,7 +22,7 @@ const Categories = () => {
       id: 3,
     },
   ];
-// seam a  d e
+// seam a  d e ew
   return (
     <div className="my-5">
       <h2 className="text-5xl text-center my-12 text-primary font-bold">
